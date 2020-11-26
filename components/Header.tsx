aaa975a4814eda1a5b592deed44c939eed558d43
Hedwig Farthofer <hedwig.farthofer@outlook.at>
2020-11-26 10:35:37 +0100

    Update tsconfig.json

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ const headerStyles = css`
 
 export default function Header() {
   return (
-    //when no line 3/4, TypeScript warning: Type '{ children: Element; css: SerializedStyles; }' is not assignable to type 'DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>'.
+    //when no line 3/4 -> TypeScript warning: Type '{ children: Element; css: SerializedStyles; }' is not assignable to type 'DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>'.
     // Property 'css' does not exist on type 'DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>'.
     <header css={headerStyles}>
       <ul>
